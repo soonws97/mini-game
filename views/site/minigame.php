e4fff4370fb0b6412c875c25403db6c67afa945f
darrensoap <darrensoap@gmail.com>
2017-07-06 14:35:47 +0800

    useless

--- views/site/minigame.php
+++ views/site/minigame.php
@@ -1,8 +1,11 @@
 ﻿<!DOCTYPE html>
-<?php 
+<?php
 use app\models\GameRecord;
 use app\models\GameResult;
 use app\models\User;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
 ?>
 <head>
 <meta charset="UTF-8">
@@ -12,7 +15,7 @@ use app\models\User;
 <body>
 	<div id="top">
 	<span id="user"><?php echo Yii::$app->session['userName'];?></span>
-	
+
 		<?php 	$today = date('Y-m-d');
 			$userName =Yii::$app->session['userName'];
 			$user = User::find()->where('userName = :name' ,[':name' => $userName])->one();
@@ -27,27 +30,27 @@ use app\models\User;
 							$nowMin = $gamedata->min_value;
 					}
 			?>
-			
+
 		<a href="<?php echo yii\helpers\Url::to(['site/logout'])?>" class="log">登出</a>
-						
-			<h1> <?php 	
+
+			<h1> <?php
 					if($gamecheck <=0 ){
 					echo "您是否是我们要找的那个有缘人呢~~~";
 						}
-			else { 					
+			else {
 					if($getToken == 0){
 						echo "恭喜你，您就是我们要找的幸运儿！";
 						}
 				}?> </h1>
 	</div>
 
-	
+
 	<div id="middle">
 		<div id="left" >
 			<h2 id="min" >
-				<?php 	
+				<?php
 				if($gamecheck <=0 ){}
-					else{ 
+					else{
 						if($val == $ansNow){
 							}
 						else{
@@ -59,12 +62,12 @@ use app\models\User;
 
 		<div id="mid">
 			<h2 id="to" >
-				<?php 
-					
+				<?php
+
 					if($gamecheck <=0 ){}
-					
+
 					else{
-						
+
 						if($val == $ansNow)
 						{
 							echo $ansNow;
@@ -78,12 +81,12 @@ use app\models\User;
 
 		<div id="right" >
 			<h2 id="max" >
-				<?php 
+				<?php
 					if($gamecheck <=0 ){}
-					
+
 					else{
 						if($val == $ansNow){}
-						
+
 						else{
 							$nowMax = $gamedata->max_value;
 							echo $nowMax;
@@ -106,8 +109,8 @@ use app\models\User;
 		<hr>
 	</div>
 		<div class="rule">
-			<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">游戏规则  </button>
-			
+			<a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">游戏规则  </a>
+
 			<div class="collapse" id="collapseExample">
 				 <div>
 					在1 - 99 之间猜个数字。每位用户只能有<mark><b>5</b></mark>次猜的机会，您只需要猜中和我们所想的一样号码，就能领取我们派送的奖励。
@@ -118,18 +121,18 @@ use app\models\User;
 		</div>
 			<div class="chg">
 			<br>
-			<?php 
-				if($gamecheck >=5){	
+			<?php
+				if($gamecheck >=5){
 						echo "您今天的次数已达成。请明天再来。";
 					}
 				elseif($gamecheck <5)
 				{
 			?>
 				您还有 <?php echo 5-$gamecheck ?> 次机会哟。
-			<?php 
+			<?php
 				}
 			?>
-			
+
 
 			</div>
 
@@ -139,7 +142,3 @@ use app\models\User;
 </body>
 
 </html>
-
-
-
-
